7e311d1785799f418b5ac82a6ce8e942f37b26ba
Andrea Schiavini <metalelf0@gmail.com>
2023-07-17 07:44:51 +0200

    feat: first commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # supermover.nvim
+
 Supermover is an UI to move the current file to a different folder.
+
+## Rationale
+
+You are editing a file. You want to move it to another directory of the current workspace. You hit `<Leader>fm` (_file move_) and a telescope dialog pops up with a searchable list of directories. You hit enter and the file is moved.
+
+You are editing a file. You want to move it to another directory of the current workspace. You hit `<Leader>fm` (_file move_) and a telescope dialog pops up with a searchable list of directories. You realize the directory you wanted still doesn't exist. You hit `<C-n>` (_new_) and enter the path to the new directory. You hit enter and the file is moved.
+
+## Setup
+
+Add the following code to your `lazy` config:
+
+```lua
+{
+  "metalelf0/supermover.nvim",
+  dependencies = {
+    "tpope/vim-eunuch",
+    "nvim-telescope/telescope.nvim"
+  },
+  opts = {
+    bindings = {
+      move_file = "<leader>fm"
+    }
+  },
+}
+```
+
+## Usage
+
+Hit `<Leader>fm`, choose the target directory, and hit enter.
+
+## Todo
+
+- [ ] allow customizing `<C-n>` telescope binding to create a new directory
+
+## Credits
+
+- Thanks @tpope for your great work!
+- Thanks @tjdevries and all the telescope contributors!
---
+++ lua/supermover/init.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+local map_helpers = require("treesitter-renamer.helpers.map")
+
+M.setup = function(opts)
+	M.set_keymaps(opts)
+end
+
+M.set_keymaps = function(opts)
+	local move_file = map_helpers.dig(opts, "bindings", "move_file") or "<leader>fm"
+	vim.keymap.set("n", move_file, M.supermove, { desc = "Move current file" })
+end
+
+M.supermove = function()
+	require("telescope.builtin").find_files({
+		find_command = { "fd", "--type", "d" },
+		attach_mappings = supermover,
+		prompt_title = "Choose target dir",
+	})
+end
+
+local function supermover(prompt_bufnr, map)
+	local actions = require("telescope.actions")
+	local action_state = require("telescope.actions.state")
+
+	actions.select_default:replace(function()
+		actions.close(prompt_bufnr)
+		local selection = action_state.get_selected_entry()
+		vim.api.nvim_command("Move " .. selection[1])
+	end)
+
+	map({ "i", "n" }, "<C-n>", function(prompt_bufnr)
+		actions.close(prompt_bufnr)
+		local selection = action_state.get_selected_entry()
+		local new_dir_name = vim.fn.input("New dir name (under " .. selection[1] .. "): ")
+
+		new_dir = vim.fs.joinpath(selection[1], new_dir_name)
+		vim.api.nvim_command("Mkdir " .. new_dir)
+		vim.api.nvim_command("Move " .. new_dir)
+	end)
+
+	return true
+end
+
+return M
